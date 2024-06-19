518020b72cc4c295c4f8d1b14edfc385484339c7
Razacki Kounasso <rkounasso@metalafrique.local>
2024-06-19 16:16:28 +0000

    update

--- src/app/tpobase-management/sketch/sketch.component.ts
+++ src/app/tpobase-management/sketch/sketch.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { Slide } from '../../payload/slide';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatMenu, MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
-import {RouterLink} from "@angular/router";
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sketch',
   standalone: true,
-  imports: [MatIconModule, MatMenuModule, MatButtonModule, RouterLink],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule, NgIf],
   templateUrl: './sketch.component.html',
   styleUrl: './sketch.component.scss'
 })
 export class SketchComponent {
   @Input({required: true}) inputData : Slide;
+  isCurrentRoute: boolean = false;
+
+  constructor(private router: Router) {
+    this.isCurrentRoute = this.router.url === "/tpo-management";
+    console.log(this.isCurrentRoute); 
+  }
 }
