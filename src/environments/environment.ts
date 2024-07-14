44fcd38681a7e0d617f1301cf06ed302ba19ea8d
Olakouns <kounassolazare@gmail.com>
2024-07-14 16:07:16 +0000

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   BASE_URL: "http://localhost:8090/api/admin",
   BASE_LOGIN_URL: "http://localhost:8090",
+  // BASE_URL: "http://ec2-54-236-180-104.compute-1.amazonaws.com:8090/api/admin",
+  // BASE_LOGIN_URL: "http://ec2-54-236-180-104.compute-1.amazonaws.com:8090",
 };
